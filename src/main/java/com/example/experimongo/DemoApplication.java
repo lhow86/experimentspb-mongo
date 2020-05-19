54048ab6088f237d1494df0e23b013a4cd6d4522
liuhao <liuhao@easipass.com>
2020-05-19 14:54:35 +0800

    init

--- src/main/java/com/example/experimongo/DemoApplication.java
+++ src/main/java/com/example/experimongo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.experimongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
